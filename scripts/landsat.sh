0c94c060884fedfce9a2aa0eefd892f120444751
Chris Holden <ceholden@gmail.com>
2025-03-20 18:09:54 -0400

    Revert to 3.5.1 since we can't upgrade hls-base (Fmask 4.7 issue)

--- scripts/landsat.sh
+++ scripts/landsat.sh
@@ -12,8 +12,6 @@ inputbucket="$INPUT_BUCKET"
 prefix="$PREFIX"
 workingdir="/var/scratch/${jobid}"
 granuledir="${workingdir}/${granule}"
-# shellcheck disable=2153
-ACCODE="LaSRC v3.5.1.0"
 
 # Remove tmp files on exit
 # shellcheck disable=2064
